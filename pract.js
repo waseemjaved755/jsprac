73f45fe9c7f0c7e528a5a27e1d219a6ebcb5100c
Muhammad Waseem Javed <waseemjaved755@gmail.com>
2023-08-05 13:16:04 +0500

    func

---
+++ pract.js
@@ -0,0 +1,17 @@
+function a()
+{
+    console.log("Waseem");
+}
+a();
+
+const Hello = (a , b) =>
+{
+    return a+b;
+}
+
+console.log(Hello(1,2));
+
+const A =( a,b) => {
+  return a+b
+}
+console.log(A(3,4));
